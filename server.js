6a9c27d049ba69d423e961cb48494f7811b2c44d
Drew-E-Wilson <dwilsrock21@gmail.com>
2021-04-28 22:56:07 -0400

    bottles of beer express app complete

--- server.js
+++ server.js
@@ -30,8 +30,44 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 
+//#3 Magic 8 ball
+app.get('/magic/:question', (req, res) => {
+    question = req.params.question
+    ballArray = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    randomResponse = Math.floor(Math.random() * ballArray.length)
+    res.send(`<h1>${question}? Magic 8 Ball says: ${ballArray[randomResponse]}</h1>`)
+})
 
 
+//#4 Pass it around
+app.get('/', (req, res) => {
+    res.send(`<html>
+        <h1>99 bottles of beer on the wall</h1>
+        <a href="/98" ><h2>Take one down, pass it around</h2></a>
+    </html>`)
+})
+
+app.get('/:number_of_bottles', (req, res) => {
+    number_of_bottles = Number(req.params.number_of_bottles)
+    nextBeer = number_of_bottles - 1;
+    if (number_of_bottles !== number_of_bottles) {
+        res.status(404).json ({
+            msg: "dude, that does make any sense"
+        })
+    } else {
+        if (nextBeer >= 0) {
+        res.send(`<html>
+        <h1>${number_of_bottles} bottles of beer on the wall</h1>
+        <a href="/${nextBeer}" ><h2>Take one down, pass it around</h2></a>
+    </html>`)
+    } else {
+        res.send(`<html>
+        <h1>${number_of_bottles} bottles of beer on the wall</h1>
+        <a href="/" ><h2>Start Over?</h2></a>
+    </html>`)
+    }
+}
+})
 
 
 // Listeners
